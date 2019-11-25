91c0e2c31abca4db83e9cae140e01c50ed517e54
Ahmed <ahmed.morsy1995@gmail.com>
2019-11-25 21:57:33 +0200

    added a couple of functions and cleaned the code structure a bit more

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from stitcher import stitcher
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+import cv2
 
 
 if __name__ == '__main__':
-    image_stitcher = stitcher()
 
-    image1 = mpimg.imread('../1.jpg')
-    plt.imshow(image1)
-    image_stitcher.first_set_of_points = plt.ginput(4)
-
-    image2 = mpimg.imread('../2.jpg')
-    plt.imshow(image2)
-    image_stitcher.second_set_of_points = plt.ginput(4)
-
+    image_stitcher = stitcher(mpimg.imread('../1.jpg'),mpimg.imread('../2.jpg'))
+    #image_stitcher.manually_collect_points(5)
+    #image_stitcher.detect_keypoints_and_features()
     image_stitcher.calculate_homography_matrix()
+
+    # draw the 2 images after being wraped using the calculated homography matrix
+    # warp = cv2.warpPerspective(image_stitcher.image1,image_stitcher.h,(640,480))
+    # plt.figure(1)
+    # plt.imshow(warp)
+    # warp2 = cv2.warpPerspective(image_stitcher.image2, image_stitcher.h, (640, 480))
+    # plt.figure(2)
+    # plt.imshow(warp2)
+    # plt.show()
--- stitcher.py
+++ stitcher.py
@@ -1,11 +1,21 @@
 import numpy as np
 import cv2
-
+import matplotlib.pyplot as plt
 
 class stitcher:
-    def __init__(self):
-        self.first_set_of_points = []
-        self.second_set_of_points = []
+
+    def __init__(self, image1, image2):
+        self.first_set_of_points = None
+        self.second_set_of_points = None
+        self.h = None
+        self.image1 = image1
+        self.image2 = image2
+
+    def manually_collect_points(self,k):
+        plt.imshow(self.image1)
+        self.first_set_of_points = plt.ginput(k)
+        plt.imshow(self.image2)
+        self.second_set_of_points = plt.ginput(k)
 
     def calculate_homography_matrix(self):
         n = len(self.first_set_of_points)
@@ -27,9 +37,38 @@ class stitcher:
             b[i*2] = current_point2[0]
             b[i*2+1] = current_point2[1]
 
+        # h1 is manually calculated homography
+        # h2 is opencv's homography
+
         h1 = np.linalg.lstsq(a, b, rcond=None)[0]
         h1 = np.insert(h1, [8], [1.0])
         h1 = h1.reshape(3,3)
         h2, status = cv2.findHomography(np.array(self.first_set_of_points), np.array(self.second_set_of_points))
         print('Our Matrix\n',h1, '\nOpenCV matrix\n', h2)
+        self.h = h2
         return h1
+
+
+    def detect_keypoints_and_features(self):
+        # instead of manually choosing some matching points, we will use opencv's method
+        descriptor = cv2.xfeatures2d.SIFT_create()
+
+        # detect keypoints and extracts local invariant descriptors
+        keypoints1, features1 = descriptor.detectAndCompute(self.image1, None)
+        keypoints2, features2 = descriptor.detectAndCompute(self.image2, None)
+
+        keypoints1 = np.float32([k.pt for k in keypoints1])
+        keypoints2 = np.float32([k.pt for k in keypoints2])
+
+        #match the points
+        matcher = cv2.DescriptorMatcher_create("BruteForce")
+        matches = matcher.knnMatch(features1, features2, 2)
+
+        good = []
+        # apply Lowe’s ratio test, which is used to determine high-quality feature matches. Typical values for Lowe’s ratio are normally in the range [0.7, 0.8].
+        for m in matches:
+            if len(m) == 2 and m[0].distance < m[1].distance * 0.75:
+                good.append((m[0].trainIdx, m[0].queryIdx))
+
+        self.first_set_of_points = np.float32([keypoints1[i] for (_, i) in good])
+        self.second_set_of_points = np.float32([keypoints2[i] for (i, _) in good])
