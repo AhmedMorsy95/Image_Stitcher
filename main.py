2ff24a53045ec67d2a3c76249cd0bd9f0556403a
Ahmed <ahmed.morsy1995@gmail.com>
2019-11-28 22:33:02 +0200

    added 1.More comments. 2.Draw function to show matches. 3.Wrap function to draw the 2 images together.

--- main.py
+++ main.py
@@ -6,16 +6,10 @@ import cv2
 
 if __name__ == '__main__':
 
-    image_stitcher = stitcher(mpimg.imread('../1.jpg'),mpimg.imread('../2.jpg'))
-    #image_stitcher.manually_collect_points(5)
-    #image_stitcher.detect_keypoints_and_features()
+    image_stitcher = stitcher(mpimg.imread('../1.jpg'), mpimg.imread('../2.jpg'))
+    # uncomment only one of the 2 methods either manually choose points or use builtin method.
+    # for the manual points it's required more than 4. However the more, the better.
+    # image_stitcher.manually_collect_points(6)
+    # image_stitcher.detect_keypoints_and_features()
     image_stitcher.calculate_homography_matrix()
-
-    # draw the 2 images after being wraped using the calculated homography matrix
-    # warp = cv2.warpPerspective(image_stitcher.image1,image_stitcher.h,(640,480))
-    # plt.figure(1)
-    # plt.imshow(warp)
-    # warp2 = cv2.warpPerspective(image_stitcher.image2, image_stitcher.h, (640, 480))
-    # plt.figure(2)
-    # plt.imshow(warp2)
-    # plt.show()
+    image_stitcher.wrap()
--- stitcher.py
+++ stitcher.py
@@ -10,6 +10,7 @@ class stitcher:
         self.h = None
         self.image1 = image1
         self.image2 = image2
+        self.matches = None
 
     def manually_collect_points(self,k):
         plt.imshow(self.image1)
@@ -18,11 +19,13 @@ class stitcher:
         self.second_set_of_points = plt.ginput(k)
 
     def calculate_homography_matrix(self):
+        # this function calculates the matrix using points calculated previously
+        # 2 methods are provided implemented method and built-in
+
         n = len(self.first_set_of_points)
         if n < 4:
             print('can not calculate need at least 4 points')
             return
-
         # link for equations : https://math.stackexchange.com/questions/494238/how-to-compute-homography-matrix-h-from-corresponding-points-2d-2d-planar-homog
         a = np.zeros((2*n,8))
         b = np.zeros((2*n,1))
@@ -43,10 +46,9 @@ class stitcher:
         h1 = np.linalg.lstsq(a, b, rcond=None)[0]
         h1 = np.insert(h1, [8], [1.0])
         h1 = h1.reshape(3,3)
-        h2, status = cv2.findHomography(np.array(self.first_set_of_points), np.array(self.second_set_of_points))
+        h2, status = cv2.findHomography(np.array(self.first_set_of_points), np.array(self.second_set_of_points), cv2.RANSAC)
         print('Our Matrix\n',h1, '\nOpenCV matrix\n', h2)
-        self.h = h2
-        return h1
+        self.h = h1
 
 
     def detect_keypoints_and_features(self):
@@ -57,18 +59,49 @@ class stitcher:
         keypoints1, features1 = descriptor.detectAndCompute(self.image1, None)
         keypoints2, features2 = descriptor.detectAndCompute(self.image2, None)
 
+        # convert to float
         keypoints1 = np.float32([k.pt for k in keypoints1])
         keypoints2 = np.float32([k.pt for k in keypoints2])
 
-        #match the points
+        # match the points
         matcher = cv2.DescriptorMatcher_create("BruteForce")
         matches = matcher.knnMatch(features1, features2, 2)
 
+        # apply Lowe’s ratio test, which is used to determine high-quality feature matches.
+        # Typical values for Lowe’s ratio are normally in the range [0.7, 0.8].
+        # this helps in false-positive match pruning.
+
         good = []
-        # apply Lowe’s ratio test, which is used to determine high-quality feature matches. Typical values for Lowe’s ratio are normally in the range [0.7, 0.8].
         for m in matches:
             if len(m) == 2 and m[0].distance < m[1].distance * 0.75:
                 good.append((m[0].trainIdx, m[0].queryIdx))
 
         self.first_set_of_points = np.float32([keypoints1[i] for (_, i) in good])
         self.second_set_of_points = np.float32([keypoints2[i] for (i, _) in good])
+        self.showMatches(good,keypoints1,keypoints2)
+        self.matches = good
+
+    def showMatches(self, matches, keypoints1, keypoints2):
+        # this function draws the 2 pictures and a line between each 2 pixels considered a match
+
+        (hA, wA) = self.image1.shape[:2]
+        (hB, wB) = self.image2.shape[:2]
+
+        # construct the new image
+        matchesImage = np.zeros((max(hA, hB), wA + wB, 3), dtype="uint8")
+        matchesImage[0:hA, 0:wA] = self.image1
+        matchesImage[0:hB, wA:] = self.image2
+
+        for (trainIdx, queryIdx) in matches:
+            ptA = (int(keypoints1[queryIdx][0]), int(keypoints1[queryIdx][1]))
+            ptB = (int(keypoints2[trainIdx][0]) + wA, int(keypoints2[trainIdx][1]))
+            cv2.line(matchesImage, ptA, ptB, (0, 255, 255), 2)
+
+        plt.imshow(matchesImage)
+        plt.show()
+
+    def wrap(self):
+        result = cv2.warpPerspective(self.image1, self.h, (self.image1.shape[1] + self.image2.shape[1], self.image1.shape[0]))
+        result[0:self.image2.shape[0], 0:self.image2.shape[1]] = self.image2
+        plt.imshow(result)
+        plt.show()
