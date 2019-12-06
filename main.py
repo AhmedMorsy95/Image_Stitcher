530c38d70775575f810232e8e86b87662abf8bb7
Nevine Said <44064365+NevineSaid@users.noreply.github.com>
2019-12-06 11:36:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,3 @@ if __name__ == '__main__':
     # image_stitcher.verify_homography()
     image_stitcher.wrap()
     image_stitcher.warp_implemented()
-#     dst_pad, warped = image_stitcher.warp_image_perspective(image_stitcher.h)
-
-    # alpha = 0.5
-    # beta = 1 - alpha
-    # blended = cv2.addWeighted(warped, alpha, dst_pad, beta, 1.0)
-    # cv2.imshow("Blended Warped Image", blended)
-    # cv2.waitKey(0)
-#     plt.figure(1)
-#     plt.imshow(dst_warped)
-#     plt.show()
-
